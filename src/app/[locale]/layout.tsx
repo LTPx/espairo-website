55f34bcf966b10bfc32042ed067d395d91a11b86
Angel Capa <ricardocmdn@gmail.com>
2025-03-31 15:39:40 -0500

    add change

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -77,7 +77,7 @@ export default async function LocaleLayout({
             contact_information={contact_information}
           />
           <App locale={locale}>{children}</App>
-          <Footer />
+          {/* <Footer /> */}
         </NextIntlClientProvider>
       </body>
     </html>
