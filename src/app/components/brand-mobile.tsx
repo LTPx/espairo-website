293270ab9c561f235adf191ab2ad7f6b6efe8ba7
Angel Capa <ricardocmdn@gmail.com>
2025-05-14 10:10:31 -0500

    fix error

--- src/app/components/brand-mobile.tsx
+++ src/app/components/brand-mobile.tsx
@@ -122,7 +122,7 @@ function BrandsMobile(props: BrandsMobileProps) {
         />
       </div>
       {filteredBrands.map((brand, index) => (
-        <div key={index} className="brand-slug-page h-[100vh]">
+        <div key={index} className="brand-slug-page h-[100dvh]">
           <BrandCard
             images={brand.images_brand}
             title={brand.title}
